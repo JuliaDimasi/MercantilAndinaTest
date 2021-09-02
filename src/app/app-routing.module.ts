b0b91cc5cc00a32fe969fcdcbf6f61b2fc9984a7
Julia Dimasi <julia.dimasi@gmail.com>
2021-09-01 23:10:38 -0300

    routing de componentes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,28 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { CoberturasComponent } from './wizard-alta/coberturas/coberturas/coberturas.component';
+import { DatoVehiculoComponent } from './wizard-alta/dato-vehiculo/dato-vehiculo/dato-vehiculo.component';
+import { DatosPersonalesComponent } from './wizard-alta/datos-personales/datos-personales/datos-personales.component';
+import { ResumenComponent } from './wizard-alta/resumen/resumen/resumen.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '', component: DatosPersonalesComponent,
+  },
+  {
+    path: 'datos-personales', component: DatosPersonalesComponent,
+  },
+  {
+    path: 'dato-vehiculo', component: DatoVehiculoComponent,
+  },
+  {
+    path: 'coberturas', component: CoberturasComponent,
+  },
+  {
+    path: 'resumen', component: ResumenComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
